3b7262f88683e1a3b09b379459a048f04426599a
Voodoo30fps <voodoo30fps@gmail.com>
2021-12-19 20:14:46 +0200

    switcher script + destination page (desktop, tablet, mobile)

--- starter-code/assets/js/nav.js
+++ starter-code/assets/js/nav.js
@@ -1,13 +1,8 @@
 const navBar = document.querySelector("nav");
 const navToggler = document.querySelector(".nav-toggler");
 
-console.log(navBar);
-console.log(navToggler);
-
 navToggler.addEventListener("click", () => {
   const visibility = navBar.getAttribute("aria-expanded");
-
-  console.log(visibility);
 
   if(visibility === "false") {
     navBar.setAttribute("aria-expanded", true);
